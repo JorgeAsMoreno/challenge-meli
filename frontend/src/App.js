881de19016f9d910dd049dac2c7702d1ead9b177
JorgeAsMoreno <solisamo6@gmail.com>
2023-09-07 18:11:35 -0600

    add dependencies and config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.scss';
+import axios from 'axios'
 function App() {
+  const [message, setMessage] = useState('default')
+  axios.get('/api/test').then(res => {
+    if (res.status === 200) {
+      setMessage(res.data)
+    }
+  }).catch(err => {
+    console.error(err)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Mensaje desde express {message}</p>
     </div>
   );
 }
