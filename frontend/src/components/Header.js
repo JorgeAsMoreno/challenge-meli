1b77c6d52908ed16e017df62cba4397e333a9041
JorgeAsMoreno <solisamo6@gmail.com>
2023-09-08 13:07:18 -0600

    feature: header
    create header structure and add styles for
    mobile and desktop

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.scss'
+import logoML from '../public/assets/ml.png'
+import searchIcon from '../public/assets/search.png'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='header_logo'>
+        <img src={logoML} alt='Mercado Libre México - Donde comprar y vender de todo' />
+      </div>
+      <div className='header_form__container'>
+        <form method='GET' role='search'>
+          <input
+            type='text'
+            aria-label='Ingresa lo que quieras encontrar'
+            placeholder='Buscar productos, marcas y más…'
+          />
+          <button type='submit'>
+            <img src={searchIcon} alt='Mercado Libre México - Buscar productos' />
+          </button>
+        </form>
+      </div>
+    </header>
+  )
+}
+
+export default Header
