fd00a310971cb4fb0b77f4c97f43b219ce4eeb12
JorgeAsMoreno <solisamo6@gmail.com>
2023-09-09 01:03:39 -0600

    feat: add pagination

--- frontend/src/components/SearchResults/SearchResults.js
+++ frontend/src/components/SearchResults/SearchResults.js
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../Card/Card'
+import { ITEMS_PER_PAGE } from '../../constants/constants'
+import ReactPaginate from 'react-paginate'
+import './searchResults.scss'
 
 const SearchResults = ({ products }) => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const startIndex = currentPage * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const currentProducts = products.slice(startIndex, endIndex)
+  const pageCount = Math.ceil(products.length / ITEMS_PER_PAGE)
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected)
+  }
   return (
     <>
       {
-        products.length > 0 ?
-        products.map(product => (
-          <Card
-            key={product.id}
-            item={product}
+        currentProducts.length > 0 ?
+        <>
+          {
+            currentProducts.map(product => (
+              <Card
+                key={product.id}
+                item={product}
+              />
+            ))
+          }
+          <ReactPaginate
+            pageCount={pageCount}
+            pageRangeDisplayed={0}
+            marginPagesDisplayed={0}
+            onPageChange={handlePageChange}
+            containerClassName={'search-paginate'}
+            activeClassName={'active'}
+            previousLabel={'Anterior'}
+            nextLabel={'Siguiente'}
+            disableInitialCallback={true}
           />
-        )) : <EmptySearch />
+        </>
+        : <EmptySearch />
       }
     </>
   )
