0b383a9b4d3b9c8c1e248afbbfe9c71842e9c5d4
JorgeAsMoreno <solisamo6@gmail.com>
2023-09-13 00:08:30 -0600

    feat: add footer component to layout

---
+++ frontend/src/components/Footer/links.js
@@ -0,0 +1,46 @@
+export const footerLinks = {
+  jobs: {
+    title: 'Trabaja con nosotros',
+    link: '/'
+  },
+  legales: {
+    title: 'Términos y condiciones',
+    link: '/'
+  },
+  privacy: {
+    title: 'Cómo cuidamos tu privacidad',
+    link: '/'
+  },
+  accessibility: {
+    title: 'Accesibilidad',
+    link: '/'
+  },
+  help: {
+    title: 'Ayuda',
+    link: '/'
+  }
+}
+
+export const footerCards = {
+  payments: {
+    image: 'https://http2.mlstatic.com/storage/homes-korriban/assets/images/ecosystem/payment.svg',
+    title: 'Elige cómo pagar',
+    description: 'Con Mercado Pago, paga con tarjeta, débito o efectivo. También puedes pagar en hasta 12 mensualidades sin tarjeta con Mercado Crédito.',
+    ctaLink: 'Cómo pagar con Mercado Pago',
+    link: '/'
+  },
+  shipping: {
+    image: 'https://http2.mlstatic.com/storage/homes-korriban/assets/images/ecosystem/shipping.svg',
+    title: 'Envío gratis desde $ 299',
+    description: 'Al registrarte en Mercado Libre tienes envíos gratis en miles de productos.',
+    ctaLink: null,
+    link: '/'
+  },
+  security: {
+    image: 'https://http2.mlstatic.com/storage/homes-korriban/assets/images/ecosystem/protected.svg',
+    title: 'Seguridad, de principio a fin',
+    description: '¿No te gusta? ¡Devuélvelo! En Mercado Libre, no hay nada que no puedas hacer, porque estás siempre protegido.',
+    ctaLink: 'Cómo te protegemos',
+    link: '/'
+  },
+}
